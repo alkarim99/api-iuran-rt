eff3cd0f32e4be20d8f31af8abdaa2d39af6909c
Abdullah Al-Karim Amrullah <abdullahalkarimamrullah01@gmail.com>
2024-04-10 14:34:45 +0700

    feat: update database config

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 const { MongoClient } = require("mongodb")
 
-const client = new MongoClient(process.env.DB_URI)
+const client = new MongoClient(
+  "mongodb+srv://abdullahalkarimamrullah01:fU4X5ttWwD6ArDpD@clusteriuranrt.at1v5e4.mongodb.net/?retryWrites=true&w=majority"
+)
 const collectionPayment = client.db("Iuran-DB").collection("payments")
 const collectionUser = client.db("User-DB").collection("user")
 const collectionWarga = client.db("Warga-DB").collection("wargas")
